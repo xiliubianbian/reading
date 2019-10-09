13f63fd4c8046a0b4c5bfaecf892449ad1c7a1a9
细刘变变 <1078937580@qq.com>
2019-10-09 21:07:43 +0800

    小程序实战项目初始化

---
+++ components/like/index.js
@@ -0,0 +1,37 @@
+// components/like/index.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        // 点赞的状态
+        like: {
+            type: Boolean,
+            value: false
+        },
+        // 点赞数
+        count: {
+            type: Number,
+            value: 0
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {},
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        onLike: function(event) {
+            let like = this.properties.like,
+                count = this.properties.count;
+            this.setData({
+                count: like ? count - 1 : count + 1,
+                like: !like
+            })
+        }
+    }
+})
